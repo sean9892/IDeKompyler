6e656d7a1e97a09907df9ec39fa02cf84eb4face
sean9892 <46587635+sean9892@users.noreply.github.com>
2022-07-26 01:10:07 +0900

    Added opcode311.py

---
+++ IDeKompyler/opcode/opcode.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class OPCODE:
+    opname : str  # name of opcode
+    oparg  : bool # existence of oparg
+    push   : int  # the number of elements that operation pushes
+    pop    : int  # the number of elements that operation pops
+
+opcode = {} # opcode to OPCODE object
+opmap  = {} # opname to OPCODE object
+
+def def_op(_opcode:int, _opname:str, _oparg:bool, _push:int, _pop:int):
+    global opcode
+    op = OPCODE(_opname,_oparg,_push,_pop)
+    opcode[_opcode] = op
+    opmap[_opname]  = op
+
+def rm_op(_opcode):
+    _opname = opcode[_opcode].opname
+    del opcode[_opcode]
+    del opmap[_opname]
+
